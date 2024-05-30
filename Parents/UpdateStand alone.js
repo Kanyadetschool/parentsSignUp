eb0daf4afd122755686335105ca815922277f9db
Kanyadetschool <oduorgeofrey141@gmail.com>
2024-05-30 04:10:47 +0300

    jjbj

--- Parents/UpdateStand alone.js
+++ Parents/UpdateStand alone.js
@@ -3,8 +3,8 @@
     function showDelayedSweetAlert() {
       Swal.fire({
         title: 'Urgent Update!',
-        text: 'KNEC School Based Assessments is set to start with practicals as from May 1st, 2024 for Grade 3 to Grade 5. Junior school {Grade 7 and 8} from 1st July 2024.This evaluation endeavor encompasses students from grade 3 to Grade 8, with the exception of Grade 6. ',
-       footer:' &copy Examination office | 2024',
+        text: 'KNEC School Based Assessments is set to start with practicals as from June 3rd, 2024 for Grade 3 ,July 1st 2024 for Grade 4 & 5. Junior school {Grade 7 and 8} from 1st July 2024.This evaluation endeavor encompasses students from grade 3 to Grade 8, with the exception of Grade 6. ',
+        footer:' &copy Examination office | 2024',
         confirmButtonText: 'OK'
       });
     }
